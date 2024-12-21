255e933a33c38bb1321c7bf755ec1d2922a6f9a5
Arnab Bhakta <ab.inpathtoadev@gmail.com>
2024-12-21 15:23:33 +0530

    Update clab-v2.sh

--- clab-v2.sh
+++ clab-v2.sh
@@ -13,7 +13,7 @@ echo -e "${BLUE}Installing Docker...${NC}"
 
 # Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install -y ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
